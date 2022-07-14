72d43a725feeafad332717b2a172b6c3daebb67b
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-07-14 12:10:46 +0300

    Второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+
+        byte firstValue = 7;
+        System.out.println(firstValue);
+        short secondValue = 243;
+        System.out.println(secondValue);
+        int thirdValue = 356_765;
+        System.out.println(thirdValue);
+        long fourthValue = 4563L;
+        System.out.println(fourthValue);
+        float fifthValue = 1.9345f;
+        System.out.println(fifthValue);
+        double sixthValue = 5.78321;
+        System.out.println(sixthValue);
+        boolean seventhValue = 10 < 5;
+        System.out.println(seventhValue);
+        char eighthValue = 43;
+        System.out.println(eighthValue);
+
+        //Задание 2
+
+        double firstBoxer = 78.2;
+        double secondBoxer = 82.7;
+
+        double summResult = firstBoxer + secondBoxer;
+        double diffResult = secondBoxer - firstBoxer;
+        System.out.println("Общий вес двух бойцов " + summResult + " кг");
+        System.out.println("Разница между весами бойцов " + diffResult + " кг");
     }
 }
